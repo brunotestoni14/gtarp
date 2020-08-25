0ee32769b4c35657e30e166e95fcba7b481d77b7
brunotestoni14 <70211933+brunotestoni14@users.noreply.github.com>
2020-08-25 18:19:11 -0300

    Add files via upload

---
+++ z-loadscreen-master/z-loadscreen-master/__resource.lua
@@ -0,0 +1,15 @@
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+loadscreen 'index.html'
+
+
+files {
+    'index.html',
+    'stylesheet.css',
+    'config.js',
+    'app.js',
+    'config.css',
+    'imgs/bg0.png', 
+    'imgs/bg1.jpg'
+}
---
+++ z-loadscreen-master/z-loadscreen-master/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 8000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "gzrWH_sQ3c8",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 60,
+    "text": {
+        "title": "Bem Vindo a DarkCity",
+        "link": "Entre no nosso discord DarkCity https://discord.gg/mgW6VAB",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.jpg']
+}
